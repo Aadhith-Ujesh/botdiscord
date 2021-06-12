9648d418b500d6fe5fdee4cf88a429a0982f5b88
Aadhith-Ujesh <ujeshm2k2@gmail.com>
2021-06-12 15:54:17 +0530

    google imdb rate

--- command/mov.js
+++ command/mov.js
@@ -1,26 +1,17 @@
-const request=require('request');
-const cheerio =require('cheerio');
-module.exports = function(msg,args){
-var link='';
-let movie=args.join("+");
-request(`https://www.imdb.com/find?q=${movie}&ref_=nv_sr_sm`,(error,response,html) => {
-    if(!error && response.statusCode==200){
-        const $ = cheerio.load(html);
-        link=$('.result_text a').attr('href');
-        console.log(link);
-        console.log(typeof(link));
-        setTimeout(func,2000);
-    }
-});
+const request = require("request");
+const cheerio = require("cheerio");
+const axios = require("axios");
 
-function func(){
-request('https://www.imdb.com/title/tt2661044/?ref_=fn_al_tt_1',(error,response,html)=>{
-            if(!error && response.statusCode==200){
-                const $ = cheerio.load(html);
-                const rating=$('.ratingValue strong')
-                console.log(rating.text());
-                msg.reply(rating.text());
-            }
-        });
-    }
+
+const getMovieRating = async (msg,args) => {
+    let movie = args.join('+');
+    await axios.get(`https://www.google.com/search?q=${movie}`)
+    .then(res => {
+        const $ = cheerio.load(res.data);
+        const rating = $(".gsrt IZACzd").text();
+        console.log(rating)
+        msg.reply(rating);
+    })
 }
+
+module.exports=getMovieRating;
