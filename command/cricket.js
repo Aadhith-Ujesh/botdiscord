42e8a11c7e9af9450e31112980efd4c65b96de18
Aadhith-Ujesh <ujeshm2k2@gmail.com>
2021-06-15 12:46:56 +0530

    updated cricket

--- command/cricket.js
+++ command/cricket.js
@@ -1,21 +1,24 @@
-const cheerio = require("cheerio")
-const fetch = require("node-fetch")
-const msg = "islamabad"
-const str = msg.charAt(0).toUpperCase() + msg.slice(1)
+const cheerio = require("cheerio");
+const fetch = require("node-fetch");
+const msg = "zimbabwe"
+const str = msg.charAt(0).toUpperCase() + msg.slice(1);
 
 function get_match_url(){
-    fetch('https://www.cricbuzz.com/')
-    .then(response => response.text())
+    fetch('https://www.espncricinfo.com/live-cricket-score')
+    .then(res => res.text())
     .then(body => {
         const $ = cheerio.load(body)
-        $('#hm-scag-mtch-blk > ul > li  a').each(function(i,ele){
-            const match = ($(this).attr("title"))
-            if(match.indexOf(str) != -1){
-                console.log(match)
-                const match_url = $(this).attr("href")
-                console.log(match_url)
+        $('a.match-info-link-FIXTURES').each(function(i,element)
+        {   
+            const match = $(element).attr('href');
+            const arr = match.split('/')[3];
+            if(arr.indexOf(msg) != -1){
+                console.log(arr,match);
+                
             }
         })
-    });
-}
+    }
+        )
+    
+};
 get_match_url()
