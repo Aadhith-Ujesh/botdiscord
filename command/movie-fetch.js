e1efcd557713beca93a1dd133a1eb55ac41f8e47
John Doe <sasti244@gmail.com>
2021-07-08 21:07:55 +0530

    Completed imdb fetching

--- command/movie-fetch.js
+++ command/movie-fetch.js
@@ -3,7 +3,8 @@ const cheerio = require("cheerio");
 const Discord = require("discord.js")
 const searchurl = "https://www.imdb.com/find?s=tt&ref_=fn_tt&q=";
 const url = 'https://www.imdb.com/'
-var details = ""
+var details = []
+var type = ""
 
 async function findRating(msg,imdbID,name){
     await fetch(`${url}${imdbID}`)
@@ -11,36 +12,77 @@ async function findRating(msg,imdbID,name){
     .then(body => {
         const $ = cheerio.load(body);
         
-        const $synopsis=$('span.GenresAndPlot__TextContainerBreakpointXL-cum89p-5.kyYEQT')
+        const $f_type = $("div.TitleBlock__TitleMetaDataContainer-sc-1nlhx7j-2.hWHMKr > ul > li:nth-child(1)");
+        type = $f_type.text();
+
+        const $synopsis=$(' div.GenresAndPlot__ContentParent-cum89p-8.bFvaWW.Hero__GenresAndPlotContainer-kvkd64-11.twqaW > p > span.GenresAndPlot__TextContainerBreakpointL-cum89p-1.gwuUFD')
         console.log($synopsis.text())
-        const syn =$synopsis.text()
+        const syn = $synopsis.text()
+        console.log(syn)
 
         const rating = $('span.AggregateRatingButton__RatingScore-sc-1ll29m0-1.iTLWoV').text()
         console.log(rating.slice(0,3))
         rat = rating.slice(0,3)
-
         $('div.PrincipalCredits__PrincipalCreditsPanelWideScreen-hdn81t-0.iGxbgr > ul > li').each(function(i,element)
         {
-            details += ($(element).text() + '\n' ) 
+            details.push($(element).text()) 
         })
         console.log(details)
+        var star = ""
 
-        const $image = $('#__next > main > div > section.ipc-page-background.ipc-page-background--base.TitlePage__StyledPageBackground-wzlr49-0.dDUGgO > div > section > div > div.TitleMainBelowTheFoldGroup__TitleMainPrimaryGroup-sc-1vpywau-1.btXiqv.ipc-page-grid__item.ipc-page-grid__item--span-2 > section:nth-child(14) > div.ipc-shoveler > div.ipc-sub-grid.ipc-sub-grid--page-span-2.ipc-sub-grid--nowrap.ipc-shoveler__grid > div:nth-child(1) > div > img')
-        const img = $image.attr('src') 
-        console.log(img)
-        
-        card(msg,img,name,syn,rat,details);
+        if(type == "TV Series"){
+            details[0] =details[0].slice(7,)
+            const str = details[1].slice(5,)
+            low = "qwertyuiopasdfghjklzxcvbnm"
+            up = "QWERTYUIOPASDFGHJKLZXCVBNM"
+            for (let i = 0 ; i < str.length ; i++){
+                star+=str[i]
+                if(low.indexOf(str[i]) != -1 && up.indexOf(str[i+1]) != -1 && str[i]!=" "){
+                    star+=" , "
+                }
+            }
+            console.log(details[0])
+            console.log(star)
+        }else{
+            details[0] = details[0].slice(8,)
+            details[1] = details[1].slice(6,)
+            const str = details[2].slice(5,)
+            low = "qwertyuiopasdfghjklzxcvbnm"
+            up = "QWERTYUIOPASDFGHJKLZXCVBNM"
+            for (let i = 0 ; i < str.length ; i++){
+                star+=str[i]
+                if(low.indexOf(str[i]) != -1 && up.indexOf(str[i+1]) != -1 && str[i]!=" "){
+                    star+=" , "
+                }
+            }
+            console.log(details[0])
+            console.log(details[1])
+            console.log(star)
+            
+        }
+        card(msg,name,syn,rat,details,star);
     });
 }
 
-function card(msg,img,name,syn,rat,details){
-    const exampleEmbed = new Discord.MessageEmbed()
-        .setTitle(name)
-        .setImage(img)
-        .setDescription(syn)
-        .addField(rat)
-        .addField(details)
-    msg.channel.send(exampleEmbed);
+function card(msg,name,syn,rat,details,star){
+    if(type == "TV Series"){
+        const series= new Discord.MessageEmbed()
+            .setTitle(name)
+            .setDescription(syn)
+            .addField("Rating",rat)
+            .addField("Creator",details[0])
+            .addField("Stars",star)
+        msg.channel.send(series);
+    }else{
+        const movie= new Discord.MessageEmbed()
+            .setTitle(name)
+            .setDescription(syn)
+            .addField("Rating",rat)
+            .addField("Director" , details[0])
+            .addField("Writer",details[1])
+            .addField("Stars",star)
+        msg.channel.send(movie);
+    }
 }
 module.exports = function(msg,args){
     fetch(`${searchurl}${args}`)
@@ -56,3 +98,9 @@ module.exports = function(msg,args){
         
 });
 }
+
+
+
+// const $image = $('#__next > main > div > section.ipc-page-background.ipc-page-background--base.TitlePage__StyledPageBackground-wzlr49-0.dDUGgO > div > section > div > div.TitleMainBelowTheFoldGroup__TitleMainPrimaryGroup-sc-1vpywau-1.btXiqv.ipc-page-grid__item.ipc-page-grid__item--span-2 > section:nth-child(14) > div.ipc-shoveler > div.ipc-sub-grid.ipc-sub-grid--page-span-2.ipc-sub-grid--nowrap.ipc-shoveler__grid > div:nth-child(1) > div > img')
+//         const img = $image.attr('src') 
+//         console.log(img)
--- command/rottentomatoes.js
+++ command/rottentomatoes.js
@@ -3,26 +3,62 @@ const cheerio = require("cheerio");
 const Discord = require("discord.js")
 const searchurl = "https://www.imdb.com/find?s=tt&ref_=fn_tt&q=";
 const url = 'https://www.imdb.com/'
+const details = [];
 
 async function findRating(imdbID,name){
     await fetch(`${url}${imdbID}`)
     .then(response => response.text())
     .then(body => {
         const $ = cheerio.load(body);
-        // const $image = $('#__next > main > div > section.ipc-page-background.ipc-page-background--base.TitlePage__StyledPageBackground-wzlr49-0.dDUGgO > div > section > div > div.TitleMainBelowTheFoldGroup__TitleMainPrimaryGroup-sc-1vpywau-1.btXiqv.ipc-page-grid__item.ipc-page-grid__item--span-2 > section:nth-child(14) > div.ipc-shoveler > div.ipc-sub-grid.ipc-sub-grid--page-span-2.ipc-sub-grid--nowrap.ipc-shoveler__grid > div:nth-child(1) > a')
-        // const imag =$image.attr('href');
-        // console.log(imag);
+        const $f_type = $("div.TitleBlock__TitleMetaDataContainer-sc-1nlhx7j-2.hWHMKr > ul > li:nth-child(1)");
+        const type = $f_type.text();
         const $synopsis=$('span.GenresAndPlot__TextContainerBreakpointXL-cum89p-5.kyYEQT')
         console.log($synopsis.text())
         $('div.PrincipalCredits__PrincipalCreditsPanelWideScreen-hdn81t-0.iGxbgr > ul > li').each(function(i,element)
         {
-            console.log($(element).text())
+            details.push($(element).text());
+            // console.log($(element).text());
+            return 0;
         })
         const rating = $('span.AggregateRatingButton__RatingScore-sc-1ll29m0-1.iTLWoV').text()
-        console.log(rating.slice(0,3))
+        // console.log(rating.slice(0,3))
         // const syn =$synopsis.text()
         // card(msg,image,name,syn);
+        console.log(type)
+        if(type == "TV Series"){
+            details[0] = "Creator" + " : " + details[0].slice(7,)
+            const str = details[1].slice(5,)
+            var star = "Stars" + " : "
+            low = "qwertyuiopasdfghjklzxcvbnm"
+            up = "QWERTYUIOPASDFGHJKLZXCVBNM"
+            for (let i = 0 ; i < str.length ; i++){
+                star+=str[i]
+                if(low.indexOf(str[i]) != -1 && up.indexOf(str[i+1]) != -1 && str[i]!=" "){
+                    star+=" , "
+                }
+            }
+            console.log(details[0])
+            console.log(star)
+        }else{
+            details[0] = "Director" + " : " + details[0].slice(8,)
+            details[1] = "Writer" + " : " + details[0].slice(6,)
+            const str = details[2].slice(5,)
+            var star = "Stars" + " : "
+            low = "qwertyuiopasdfghjklzxcvbnm"
+            up = "QWERTYUIOPASDFGHJKLZXCVBNM"
+            for (let i = 0 ; i < str.length ; i++){
+                star+=str[i]
+                if(low.indexOf(str[i]) != -1 && up.indexOf(str[i+1]) != -1 && str[i]!=" "){
+                    star+=" , "
+                }
+            }
+            console.log(details[0])
+            console.log(details[1])
+            console.log(star)
+        }
+        
     });
+
 }
 
 // function card(msg,image,name,syn){
@@ -33,7 +69,7 @@ async function findRating(imdbID,name){
 //     msg.channel.send(exampleEmbed);
 // }
 function imdb(){
-    args="thupakki"
+    args="breakingbad"
     fetch(`${searchurl}${args}`)
     .then(response => response.text())
     .then(body => {
